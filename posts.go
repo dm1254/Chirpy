07ab2f2dddf8ee1b54cb4017d951608f837c38af
dm1254 <d.moreno1272@gmail.com>
2025-03-20 22:34:13 -0700

    Add posts.go

---
+++ posts.go
@@ -0,0 +1,125 @@
+package main 
+import(	
+	"net/http"
+	"encoding/json"
+	"time"
+	"log"
+	"github.com/google/uuid"
+	"workspace/github.com/dm1254/Chirpy/internal/database"
+	"workspace/github.com/dm1254/Chirpy/internal/auth"
+	"errors"
+)
+
+
+func(c *ApiConfig) handlerChirps (w http.ResponseWriter, r *http.Request){
+	type requestData struct{
+		Body string `json:"body"`
+	}
+	type responseData struct{
+		ID uuid.UUID `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		Body string `json:"body"`
+		UserID uuid.UUID `json:"user_id"`
+	}		
+	decoder := json.NewDecoder(r.Body)
+	reqData := requestData{}
+	err := decoder.Decode(&reqData)
+	if err != nil{
+		respondWithError(w,http.StatusInternalServerError, "Couldn't decode parameters", err)
+		return 
+	}
+	getToken,err := auth.GetBearerToken(r.Header)
+
+	if err != nil{
+		respondWithError(w,http.StatusInternalServerError, "Error getting token from header",err)
+		return 
+	}
+	validUser,err := auth.ValidateJWT(getToken,c.JWTSecret)	
+	if err != nil{
+		respondWithError(w,http.StatusUnauthorized, "Couldnt validate user", err)
+		return
+	}
+	if len(reqData.Body) <= 140{
+		params := database.CreatePostsParams{
+			Body: reqData.Body,
+			UserID: validUser,
+		}
+		postsData,err := c.db.CreatePosts(r.Context(), params)
+		if err != nil{
+			log.Printf("Couldn't create post: %s", err)
+			return 
+		}
+		respondWithJson(w,http.StatusCreated, responseData{
+			ID: postsData.ID,
+			CreatedAt: postsData.CreatedAt,
+			UpdatedAt: postsData.UpdatedAt,
+			Body: postsData.Body,
+			UserID: postsData.UserID,
+
+		})
+		return
+	}
+	err = errors.New("validation failed: post too long")
+	respondWithError(w,http.StatusBadRequest,"Post cannot exceed 140 characters" ,err )
+	return 
+}
+
+func (c *ApiConfig) handlerGetAllChirps(w http.ResponseWriter, r *http.Request){
+	type responseData struct{
+		ID uuid.UUID `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		Body string `json:"body"`
+		UserID uuid.UUID `json:"user_id"`
+	}
+	posts,err := c.db.GetPosts(r.Context())
+	if err != nil{
+		log.Printf("Error creating posts: %s",err)
+	}
+	allPosts := []responseData{}
+	for _, post := range posts{
+		postsData := responseData{
+			ID: post.ID,
+			CreatedAt: post.CreatedAt,
+			UpdatedAt:post.UpdatedAt,
+			Body: post.Body,
+			UserID:post.UserID,
+		}
+		allPosts = append(allPosts, postsData)
+	}
+	respondWithJson(w,http.StatusOK, allPosts)
+	return 
+
+}
+
+func(c *ApiConfig) handlerGetIdChirp(w http.ResponseWriter, r *http.Request){
+	postURLId := r.PathValue("chirpsID")
+	postID,err := uuid.Parse(postURLId)
+	log.Printf("postID:%s\n",postURLId)
+	if err != nil{
+		respondWithError(w,http.StatusInternalServerError, "could not convert post id to uuid", err)
+		return 
+	}
+	type responseData struct{
+		ID uuid.UUID `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		Body string `json:"body"`
+		UserID uuid.UUID `json:"user_id"`
+	}
+	post,err := c.db.GetSinglePost(r.Context(),postID)
+	if err != nil{
+		respondWithError(w, http.StatusNotFound,"Post does not exist", err)
+		return 
+	}
+	respondWithJson(w, http.StatusOK, responseData{
+		ID: post.ID,
+		CreatedAt: post.CreatedAt,
+		UpdatedAt: post.UpdatedAt,
+		Body: post.Body,
+		UserID: post.UserID,
+	})
+	return
+}
+
