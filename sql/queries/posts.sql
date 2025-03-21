edc38f8904d52d4adf3d91f2c0314425bd76fd4e
dm1254 <d.moreno1272@gmail.com>
2025-03-20 22:55:06 -0700

    sql query/schema

---
+++ sql/queries/posts.sql
@@ -0,0 +1,19 @@
+-- name: CreatePosts :one 
+INSERT INTO posts(id,created_at,updated_at,body,user_id)
+values(
+	gen_random_uuid(),
+	NOW(),
+	NOW(),
+	$1,
+	$2
+)
+RETURNING *;
+
+-- name: GetPosts :many
+SELECT * FROM posts
+ORDER BY created_at ASC; 
+
+-- name: GetSinglePost :one
+SELECT * FROM posts
+WHERE id = $1;
+
---
+++ sql/queries/users.sql
@@ -0,0 +1,22 @@
+-- name: CreateUsers :one
+INSERT INTO users (id,created_at,updated_at,email,hashed_password)
+values(
+	gen_random_uuid(),
+	NOW(),
+	NOW(),
+	$1,
+	$2
+)
+RETURNING *; 
+
+-- name: Reset :exec 
+DELETE FROM users;
+
+-- name: GetUserPass :one
+SELECT * FROM users
+WHERE email = $1;
+
+-- name: UpdateUserEmailAndPass :one
+UPDATE users 
+SET email = $1, hashed_password = $2
+RETURNING *; 
